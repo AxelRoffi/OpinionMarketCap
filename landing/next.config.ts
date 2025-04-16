0ea4a5938c1f8087f35e5d104560afcea05b022c
Axel Roffi <axelroffi@mbp-axel-roffi.home>
2025-04-16 16:17:22 +0200

    Disable ESLint during builds

--- landing/next.config.ts
+++ landing/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  eslint: {
+    // This completely disables ESLint during builds
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
